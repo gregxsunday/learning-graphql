6c17d50147c049baa2a7bc06a51b8f23b8251988
gregxsunday <gregxsunday@gmail.com>
2022-08-01 11:27:21 +0200

    init

---
+++ index.js
@@ -0,0 +1,62 @@
+const { ApolloServer, gql } = require('apollo-server');
+const { ApolloServerPluginLandingPageGraphQLPlayground } = require("apollo-server-core");
+
+// A schema is a collection of type definitions (hence "typeDefs")
+// that together define the "shape" of queries that are executed against
+// your data.
+const typeDefs = gql`
+  # Comments in GraphQL strings (such as this one) start with the hash (#) symbol.
+
+  # This "Bug" type defines the queryable fields for every vuln in our data source.
+
+  type Bug {
+    bugClass: String
+    payloads: [String]
+  }
+
+  # The "Query" type is special: it lists all of the available queries that
+  # clients can execute, along with the return type for each. In this
+  # case, the "bugs" query returns an array of zero or more Vulnerabilities (defined above).
+  type Query {
+    bugs: [Bug]
+  }
+`;
+
+const bugs = [
+    {
+      bugClass: 'XSS',
+      payloads: [
+        '<img src=x onerror=alert(document.domain)>', '<script>alert(document.domain)</script>'
+      ],
+    },
+    {
+      bugClass: 'SQLi',
+      payloads: [
+        '" OR 1=1 --'
+      ],
+    },
+  ];
+
+  // Resolvers define the technique for fetching the types defined in the
+// schema. This resolver retrieves books from the "books" array above.
+const resolvers = {
+    Query: {
+      bugs: () => bugs,
+    },
+  };
+
+  // The ApolloServer constructor requires two parameters: your schema
+// definition and your set of resolvers.
+const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+    csrfPrevention: true,
+    plugins: [
+        ApolloServerPluginLandingPageGraphQLPlayground()
+    ]
+  });
+  
+  // The `listen` method launches a web server.
+  server.listen().then(({ url }) => {
+    console.log(`🚀  Server ready at ${url}`);
+  });
